99f71eb8a3af4431ecd77595e6c6741ef853ad75
tkgstrator <nasawake.am@gmail.com>
2022-06-19 05:20:24 +0900

    - Add @ionic/pwa-elements

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from "./router";
 // Added by the CLI
 import "./registerServiceWorker";
 
+// Above the createApp() line
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+
 import { IonicVue } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
@@ -24,6 +27,9 @@ import "@ionic/vue/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 
 const app = createApp(App).use(IonicVue).use(router);
 
