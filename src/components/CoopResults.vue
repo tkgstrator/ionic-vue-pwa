021487f5e6eae9b81e10b302944e00ccea083600
tkgstrator <nasawake.am@gmail.com>
2022-06-22 21:22:31 +0900

    - 一覧の表示を変更

--- src/components/CoopResults.vue
+++ src/components/CoopResults.vue
@@ -14,12 +14,24 @@ interface Player {
   grade_point?: number
 }
 
+interface Wave {
+  event_type: number
+  water_level: number
+  golden_ikura_num: number
+  golden_ikura_pop_num: number
+  ikura_num: number
+  quota_num: number
+}
+
 interface Result {
   salmon_id: number
-  job_results: JobResult
+  job_result: JobResult
+  golden_ikura_num: number
+  ikura_num: number
+  no_night_waves: boolean
   danger_rate: number
   play_time: string
-  players: Player[]
+  waves: Wave[]
 }
 
 export default defineComponent({
@@ -47,13 +59,53 @@ export default defineComponent({
   },
   methods: {
     async onReload() {
-      const url = `${process.env.VUE_APP_SERVER_URL}/${process.env.VUE_APP_SERVER_API_VER}/results`;
+      const parameters = {
+        "offset": "0",
+        "limit": "200",
+        "include_details": false.toString(),
+        "order": false.toString()
+      }
+      const query = new URLSearchParams(parameters);
+      const url = `${process.env.VUE_APP_SERVER_URL}/${process.env.VUE_APP_SERVER_API_VER}/results?${query}`;
       fetch(url).then(response => response.json()).then(response => {
         this.results = response.results;
+        console.log(this.results)
       });
     },
     onRefresh(event: CustomEvent) {
       this.onReload().then(() => event.detail.complete());
+    },
+    getWaterLevel(water_level: number): string {
+      switch (water_level) {
+        case 0:
+          return "LT"
+        case 1:
+          return "NT"
+        case 2:
+          return "HT"
+        default:
+          return "NT"
+      }
+    },
+    getEventType(event_type: number): string {
+      switch (event_type) {
+        case 0:
+          return "-"
+        case 1:
+          return "Rush"
+        case 2:
+          return "Goldie Seeking"
+        case 3:
+          return "Griller"
+        case 4:
+          return "The Mothership"
+        case 5:
+          return "Fog"
+        case 6:
+          return "Cohock Charge"
+        default:
+          return "-"
+      }
     }
   },
 });
@@ -69,15 +121,32 @@ export default defineComponent({
         <ion-label>
           <section class="coop-result-summary">
             <div class="coop-result-job-result">
-              <h3>{{ result.salmon_id }}</h3>
-              <h2>{{ result.danger_rate }}%</h2>
+              <ion-label class="salmon_id">{{ result.salmon_id }}</ion-label>
+              <ion-label :class="result.job_result.is_clear ? 'job_clear' : 'job_failure'">{{ result.job_result.is_clear
+                  ?
+                  "Clear" : "Failure"
+              }}</ion-label>
+              <ion-label class="danger_rate">{{ result.danger_rate.toFixed(1) }}</ion-label>
             </div>
-            <div class="coop-result-player-list">
-              <ul>
-                <li class="coop-result-player-list-item" v-for="player in result.players" :key="player.nsaid">
-                  <label>{{ player.name }}</label>
-                </li>
-              </ul>
+            <div class="coop-result-job-waves">
+              <div class="coop-result-job-waves-golden-ikura">
+                <template v-for="wave in result.waves" :key="wave">
+                  <ion-label>{{
+                      ("00" + wave.golden_ikura_num).slice(-2)
+                  }}/{{ wave.quota_num }}</ion-label>
+                </template>
+              </div>
+              <div class="coop-result-job-waves-information">
+                <template v-for="wave in result.waves" :key="wave">
+                  <ion-label class="wave-information">{{ getWaterLevel(wave.water_level) }} {{
+                      getEventType(wave.event_type)
+                  }}</ion-label>
+                </template>
+              </div>
+            </div>
+            <div class="coop-result-job-detail">
+              <ion-label class="num golden_ikura_num">{{ result.golden_ikura_num }}</ion-label>
+              <ion-label class="num ikura_num">{{ result.ikura_num }}</ion-label>
             </div>
           </section>
         </ion-label>
@@ -89,21 +158,52 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 ion-label {
-  margin: 4px 0;
+  font-size: 0.8rem !important;
+  font-family: "Noto Sans JP" !important;
+  margin: 0 auto !important;
+
+  &.salmon_id {
+    color: grey;
+    font-size: 12px !important;
+  }
+
+  &.job_clear {
+    color: #39e464;
+    font-weight: bold;
+  }
+
+  &.job_failure {
+    color: #ff7500;
+    font-weight: bold;
+  }
+
+  &.danger_rate {
+    font-size: 0.75rem !important;
+
+    &::after {
+      content: "%";
+    }
+  }
+
+  &.golden_ikura_num {
+    font-size: 0.9rem !important;
+    color: yellow;
+    font-weight: bold;
+  }
+
+  &.ikura_num {
+    font-size: 0.9rem !important;
+    color: red;
+    font-weight: bold;
+  }
+
+  &.num {
+    &::before {
+      content: "x";
+    }
+  }
 }
 
-h2 {
-  font-size: 12px;
-}
-
-h3 {
-  color: gray;
-  font-size: 11px;
-}
-
-label {
-  font-size: 12px;
-}
 
 .coop-result {
   max-width: 440px;
@@ -120,49 +220,30 @@ label {
 
 .coop-result-job-result {
   width: 20%;
-  max-width: 80px;
+  min-width: 60px;
   padding: 2px 4px;
-
-  // &::before {
-  //     position: absolute;
-  //     display: block;
-  // }
 }
 
-.coop-result-player-list {
-  display: block;
-  width: 80%;
-  max-width: 340px;
-  // list-style: none;
+.coop-result-job-waves {
+  width: 100%;
+  display: flex;
+  padding: 2px 4px;
 
-  // &::before {
-  //     position: absolute;
-  // }
-
-  ul {
-    display: block;
-    max-width: 320px;
-    margin: 0 auto;
-    margin-right: 10px;
-    right: 0;
-    column-count: 2;
-    // justify-content: right;
-    // align-items: right;
-    text-align: left;
+  .coop-result-job-waves-golden-ikura {
+    // width: 100%;
+    padding-right: 10px;
+    text-align: right;
   }
 }
 
-.coop-result-player-list-item {
-  list-style: none;
-  display: block;
-  // display: flex;
+.coop-result-job-detail {
+  width: 14%;
+  padding: 2px 4px;
+  text-align: right;
+}
 
-  // &li {
-  //     text-align: left;
-  // }
-
-  // &::before {
-  //     position: absolute;
-  // }
+.wave-information {
+  margin-left: 4px;
+  margin-right: 4px;
 }
 </style>
